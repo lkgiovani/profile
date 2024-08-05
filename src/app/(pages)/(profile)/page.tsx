b369fa8e73f06332a64ca6c5ec323f5f24af2aa3
giovani souza <lkgiovani@gmail.com>
2024-08-05 20:00:55 -0300

    feat: cofig básica

---
+++ src/app/(pages)/(profile)/page.tsx
@@ -0,0 +1,5 @@
+const Profile = () => {
+  return <div>salve</div>;
+};
+
+export default Profile;
---
+++ src/app/(pages)/layout.tsx
@@ -0,0 +1,14 @@
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Raise Tips",
+  description: "O seu site de análises",
+};
+
+export default function pageLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return <div className="flex w-full h-screen">{children}</div>;
+}
